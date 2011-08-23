86bd712e5dee74c9e1e2e82a7b20678131ac4f97
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-08-23 13:10:28 +0300

    first commit

---
+++ src/regex/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regex;
+
+/**
+ *
+ * @author mihnea
+ */
+public class Main {
+    public static void main(String[] args) {
+        GUI g = new GUI();
+        g.setVisible(true);
+    }
+}
